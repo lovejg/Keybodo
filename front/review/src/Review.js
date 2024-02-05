41ea551791859de27887c414559b92fbef319b25
tmakdrl <tmakdrl@naver.com>
2024-02-05 14:59:31 +0900

    connect review-DB

--- front/review/src/Review.js
+++ front/review/src/Review.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
+import { database } from './firebase_DB';
+import { ref, push, set, serverTimestamp } from 'firebase/database';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './Review.css';
 
-const RatingWithReview = ({ totalStars = 5, initialRating = 0 }) => {
+const RatingWithReview = ({ totalStars = 5, initialRating = 1 }) => { // 5점 만점, 최소는 1점(초기값)
   const [rating, setRating] = useState(initialRating);
   const [review, setReview] = useState('');
 
@@ -15,11 +19,21 @@ const RatingWithReview = ({ totalStars = 5, initialRating = 0 }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // 리뷰 제출
-    console.log(`Rating: ${rating}, Review: ${review}`);
-    // 제출 후 초기화
-    setReview('');
-    setRating(0);
+    // Firebase Realtime Database에 리뷰 데이터 저장
+    const reviewRef = push(ref(database, 'reviews')); // 'reviews'는 데이터를 저장할 경로
+    set(reviewRef, {
+      rating,
+      review,
+      createdAt: serverTimestamp() // 리뷰 생성 시간(걍 기본데이터로 많이 쓰이길래 넣어봄)
+    }).then(() => {
+      toast.success('제출 성공!', { // 토스트 메세지로 제출 성공 표시
+        onClose: () => { // 닫기 버튼 누를 시에
+          window.location.reload(); // 페이지 새로고침
+        } // onClose가 아닌 setTimeout으로 delay를 주면 토스트 메세지를 무시하고 새로고침 되는 문제가 생겼음
+      });
+    }).catch(error => { // 에러 발생 시
+      toast.error('제출 실패: ' + error.message); // 토스트 메세지로 에러 표시
+    });
   };
 
   return (
@@ -54,8 +68,9 @@ const RatingWithReview = ({ totalStars = 5, initialRating = 0 }) => {
         ></textarea>
         <button type="submit" className="submit-btn">제출</button>
       </form>
+      <ToastContainer position="top-center" autoClose={5000} />
     </div>
-  );
+  );  
 };
 
 export default RatingWithReview;
