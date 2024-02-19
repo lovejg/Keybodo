402625e0ed86c756ae9198739181705fe2f50afa
tmakdrl <tmakdrl@naver.com>
2024-02-19 09:44:35 +0900

    update info page

--- front/main/src/App.js
+++ front/main/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import MainPage from './Main'; // 메인 페이지 컴포넌트
-import InfoPage from './Info'; // 상세 페이지 컴포넌트
+import Main from './Main'; // 메인 페이지 컴포넌트
+import Info from './Info'; // 상세 페이지 컴포넌트
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/detail/:id" element={<InfoPage />} />
+        <Route path="/" element={<Main />} />
+        <Route path="/detail/:id" element={<Info />} />
       </Routes>
     </Router>
   );
