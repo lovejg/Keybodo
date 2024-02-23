87b27c041baf01d3123aa3cd6a6c4f54d0a2adc7
tmakdrl <tmakdrl@naver.com>
2024-02-23 20:58:46 +0900

    erase review project

--- view.sql
+++ view.sql
@@ -1,6 +1,6 @@
 use keybodo;
 
--- drop view `V_switches`;
+drop view `V_switches`;
 
 CREATE View V_switches AS
 SELECT t_sw.switch_id,t_sw.switch_name,t_sw.switch_method,t_sw.switch_type,t_sp.gc_sf as spring_force,t_sw.switch_pitch,t_sw.switch_price,t_sw.maker,t_sw.infolink FROM `T_switch` t_sw
